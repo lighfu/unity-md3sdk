3fed9f42f4d675782160b356f3b4b8ced4c103a0
lighfu <82653778+lighfu@users.noreply.github.com>
2026-04-16 20:21:21 +0900

    chore: bump version to 0.8.1

--- MD3SDKSettingsWindow.cs
+++ MD3SDKSettingsWindow.cs
@@ -9,7 +9,7 @@ namespace AjisaiFlow.MD3SDK.Editor
 {
     public class MD3SDKSettingsWindow : EditorWindow
     {
-        const string Version = "0.8.0";
+        const string Version = "0.8.1";
 
         MD3Theme _theme;
         VisualElement _iconContainer;
